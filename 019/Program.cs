ef0f2dac3a5e687bd3688c68cd540952dc6aa93b
Ilgova <ilgova.1594@mail.ru>
2023-02-01 00:11:17 +0300

    Homework003

---
+++ 019/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом (НЕ использовать число как строку, то есть сравнения типа number[0] == number[4] делать НЕЛЬЗЯ. Используем операторы % и /).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int num = Number / 1000;
+int rewnumb = Number % 100;
+rewnumb = rewnumb % 10 * 10 + rewnumb / 10;
+
+if (num == rewnumb)
+Console.WriteLine("ДА");
+else 
+Console.WriteLine("Нет");
+
+
+
+
+
+
+
+
+
---
+++ 021/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int x1 = index("x", "A");
+int y1 = index("y", "A");
+int z1 = index("z", "A");
+int x2 = index("x", "B");
+int y2 = index("y", "B");
+int z2 = index("z", "B");
+
+int index(string coordinate, string point)
+{
+    Console.Write($"Введите координату {coordinate} точки {point}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double distance = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+
+Console.WriteLine($"Длина отрезка: {Math.Round(distance , 2)}");
